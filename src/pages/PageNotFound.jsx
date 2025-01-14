fecdc7a9f0a80023b6ab9a7ac1078d76267718b8
unknown <marko.yanovych.pz.2020@lpnu.ua>
2025-01-14 18:11:26 +0200

    fix PageNotFound page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,7 @@
-export default function PageNotFound() {
-  return <div>PageNotFound</div>;
-}
+const PageNotFound = () => (
+  <div className="flex flex-col h-screen justify-center items-center space-y-4">
+    <h1 className="font-bold text-3xl md:text-5xl">Page is not found</h1>
+  </div>
+);
+
+export default PageNotFound;
